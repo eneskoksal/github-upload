16e7d14c8604a7a542a9f8db69ee11a51f162b3d
eneskoksal <talhaeneskoksal@gmail.com>
2021-04-03 21:49:47 +0300

    Initial Commit

---
+++ tek-bankatm/src/main/java/com/iyte/bankatm/tek_bankatm/App.java
@@ -0,0 +1,13 @@
+package com.iyte.bankatm.tek_bankatm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
